23de6d2ec26d551a0b171f3fe2be28853abf57cb
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-11 09:20:07 +0100

    Add Peregrine directives [skip ci]

--- scripts/rerun.sh
+++ scripts/rerun.sh
@@ -7,6 +7,15 @@
 #   ./scripts/rerun.sh
 #
 #
+#SBATCH --partition=gelifes
+#SBATCH --time=10:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=10G
+#SBATCH --job-name=run_example_30
+#SBATCH --output=run_example_30.log
+#
 rm -rf example_30_314
 Rscript example_30.R
 
