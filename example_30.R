da34cbdde719d9ca943487db83c6f99bd62bc424
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-26 15:28:00 +0200

    Re-run to have same settings as the example (pirouette_example_28) with 100 replicates

--- example_30.R
+++ example_30.R
@@ -12,7 +12,30 @@ rng_seed <- 314
 
 # Create phylogenies
 set.seed(rng_seed)
+
+# Nice, but does not match example 30
 phylogeny <- create_exemplary_dd_tree(n_taxa = n_taxa, crown_age = crown_age)
+
+# Create simulation function like example 28
+sim_dd_tree_fun <- function(crown_age) {
+  extinction_rate <- 0.1
+  n_taxa <- 6
+  n_0 <- 2 # Initial number of species at stem/crown of tree
+  diff <- (log(n_taxa) - log(n_0)) / crown_age
+  speciation_rate <- 3.0 * (diff + extinction_rate)
+  carrying_capacity <- n_taxa # clade-level
+  dd_parameters <- c(speciation_rate, extinction_rate, carrying_capacity)
+  ddmodel <- 1 # linear dependence in speciation rate with parameter K
+  dd_sim_result <- DDD::dd_sim(pars = dd_parameters, age  = crown_age, ddmodel = ddmodel)
+  phylogeny <- dd_sim_result$tes # Only extant species
+  phylogeny
+}
+sim_tree_fun <- pryr::partial(
+  sim_dd_tree_fun,
+  crown_age = crown_age
+)
+phylogeny <- sim_tree_fun()
+
 
 # Setup pirouette
 pir_params <- create_std_pir_params(folder_name = folder_name)
